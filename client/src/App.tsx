59277181ed6d4db8cc4f430841ad0e1530993f94
Alsecode <adementor@yandex.ru>
2025-02-27 12:55:49 +0300

    Реализована основная логика приложения

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,99 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import FileUpload from './components/FileUpload/FileUpload';
+import ButtonCommon from './components/ButtonCommon/ButtonCommon';
+import logo from './img/logo.png';
+import PlateAnswer from './components/PlateAnswer/PlateAnswer';
 
+interface ResponseData {
+  results: {
+    plate: string,
+    region: {
+      code: string,
+    }
+  }[]
+}
+
+interface AnswerItem {
+  plate: string;
+  region: string;
+}
 
 const App = () => {
-  const [message, setMessage] = useState('');
+  // Выбранный файл
+  const [file, setFile] = useState<File | null>(null);
 
-  useEffect(() => {
-    // Выполняем запрос к API-серверу
-    fetch('http://localhost:5000/api/hello')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(err => console.error('Ошибка при запросе к серверу:', err));
-  }, []);
+  // Состояния кнопки
+  const [isActive, setActive] = useState<boolean>(false);
+  const [isDisabled, setDisabled] = useState<boolean>(true);
+
+  // Ответ
+  const [answer, setAnswer] = useState<AnswerItem[] | null>(null);
+  // Текст возникшей ошибки
+  const [error, setError] = useState<string>('');
+
+  // Обработчик изменения файла
+  const handleChangeFile = (file: File) => {
+    // Обнуляем предыдущий ответ и ошибку
+    setAnswer(null);
+    setError('');
+
+    setFile(file);
+    // Кнопка становится разблокированной
+    setDisabled(false);
+  }
+
+  const handleClick = async () => {
+    // Установка кнопки активной и заблокированной во время запроса
+    setActive(true);
+    setDisabled(true);
+
+    // Обнуляем предыдущий ответ и ошибку
+    setAnswer(null);
+    setError('');
+
+    // Добавление файла изображения в форму
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const res = await fetch('http://localhost:5000/api/upload', {
+        method: 'POST',
+        body: formData,
+      });
+      const { results }: ResponseData = await res.json();
+      const formattedResult = results.map(result => 
+        ({ plate: result.plate, region: result.region.code })
+      );
+      formattedResult.length ? setAnswer(formattedResult) : setError('К сожалению не удалось определить номер. Попробуйте другую фотографию');
+    } catch (error) {
+      console.error(error);
+      setError('Произошла ошибка при отправке файла');
+    } finally {
+      setActive(false);
+      setDisabled(false);
+    }
+  };
+
 
   return (
     <div className="container">
-      <h1 className="title">Привет!</h1>
-      <p>Сообщение с сервера: {message}</p>
+      <main className="main-content">
+        <img className="logo main-content__logo" src={logo} alt="logo"></img>
+        <h1 className="title main-content__title">Определение номера автомобиля по фотографии</h1>
+        <FileUpload className="fileUpload" onFileChange={handleChangeFile} />
+        <div className="btn-container">
+          <ButtonCommon isActive={isActive} disabled={isDisabled} onClick={handleClick}>Определить</ButtonCommon>
+        </div>
+        {answer ? (
+          <div className="plate-answer">
+            {answer.map(answerItem => (
+              <PlateAnswer key={answerItem.plate} answer={answerItem} />
+            ))}
+          </div>
+        ) : null}
+        {error ? <p className="plate-error">{error}</p> : null}
+      </main>
     </div>
   );
 };
